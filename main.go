dcf1c9f80a74b89b9fdc4f8e9a14d2e525631f3e
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2018-12-13 14:14:56 +0100

    rest: expose vms and healthcheck endpoints
    This PR provides ability to expose functionality as rest. We introduce
    two endpoints:
    - healthcheck
    - getVMs - returns list of vm names
    
    
    Signed-off-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>

--- main.go
+++ main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/pkliczewski/provider-pod/client"
 )
 
 func main() {
+	port := getEnv("SERVER_PORT", "8080")
+	var router = mux.NewRouter()
+
+	router.HandleFunc("/healthcheck", healthCheck).Methods("GET")
+	router.HandleFunc("/vms", GetVMs).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
+}
+
+func GetVMs(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 
 	c, err := client.NewClient(ctx)
 	if err != nil {
 		log.Fatal(err)
+		respondWithError(w, http.StatusFailedDependency, err.Error())
+		return
 	}
 
 	defer c.Logout(ctx)
@@ -21,10 +37,36 @@ func main() {
 	vms, err := c.GetVMs(ctx)
 	if err != nil {
 		log.Fatal(err)
+		respondWithError(w, http.StatusFailedDependency, err.Error())
+		return
 	}
 
-	// Print summary per vm (see also: govc/vm/info.go)
-	for _, vm := range vms {
-		fmt.Printf("%s: %s\n", vm.Summary.Config.Name, vm.Summary.Config.GuestFullName)
+	names := make([]string, len(vms))
+	for i, vm := range vms {
+		names[i] = vm.Summary.Config.Name
 	}
+	respondWithJSON(w, http.StatusOK, map[string][]string{"result": names})
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "OK"})
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
